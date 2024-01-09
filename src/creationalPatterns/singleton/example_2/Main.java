9b4bd18dbfc77fae67bef6801e7eee0b3e06b857
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-01-09 23:34:14 +0600

    2nd example of Singleton

--- src/creationalPatterns/singleton/example_2/Main.java
+++ src/creationalPatterns/singleton/example_2/Main.java
@@ -2,8 +2,8 @@ package creationalPatterns.singleton.example_2;
 
 public class Main {
     public static void main(String[] args) {
-        Singleton singleton = Singleton.getInstance("Это синглтон");
-        Singleton anotherSingleton = Singleton.getInstance("Это не синглтон");
+        Singleton singleton = Singleton.getInstance("Я - реализация синглтона");
+        Singleton anotherSingleton = Singleton.getInstance("Возможно я тоже являюсь синглтоном");
         System.out.println(singleton.value);
         System.out.println(anotherSingleton.value);
     }
